353cac357a116e1b7acd798b99837a8de412efde
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-24 19:49:15 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
   
-  validates :user,            presence: true
+  validates :nickname,        presence: true
   validates :genre_id,        presence: true
 end
