8a28ea4827c33b0252737d777eab57e33f6c5261
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-25 14:55:36 +0900

    createアクションとストロングパラメーターを定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,20 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :genre_id, :impression, :sentence, :synopsis, :begining).merge(user_id: current_user.id)
+  end
+
 end
