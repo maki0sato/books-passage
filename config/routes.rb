60155ca57a5ef129870c93411c5ae5656646628d
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-25 11:35:40 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'books/index'
   root to: "books#index"
-  resources :books, only: [:index, new]
+  resources :books, only: [:index, :new]
 end
