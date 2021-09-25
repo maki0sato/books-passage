975d9c9a532fe6f6a029db0970ee66a466586e57
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-25 12:03:41 +0900

    バリデーションを設置

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,15 @@ class Book < ApplicationRecord
   belongs_to :user
   belongs_to :genre
   has_one_attached :image
+
+
+  validates :title,          presence: true
+  validates :author,         presence: true
+  validates :genre_id,       presence: true
+  validates :impression,     presence: true
+  validates :sentence,       presence: true
+  validates :synopsis,       presence: true
+  validates :begining,       presence: true
+  validates :user_id,        presence: true
+  validates :image,          presence: true
 end
