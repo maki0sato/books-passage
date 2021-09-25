5f72655b125d298b076a45ae47de6459faa07dff
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-25 11:11:28 +0900

    booksテーブル作成

--- db/migrate/20210925015604_create_books.rb
+++ db/migrate/20210925015604_create_books.rb
@@ -2,6 +2,15 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
 
+      t.string     :title,       null:false
+      t.string     :author,      null:false
+      t.integer    :genre_id,    null:false
+      t.text       :impression,  null:false
+      t.string     :sentence,    null:false
+      t.text       :synopsis,    null:false
+      t.string     :begining,    null:false
+      t.references :user,        null:false, foreign_key: true
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_070123) do
+ActiveRecord::Schema.define(version: 2021_09_25_015604) do
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "author", null: false
+    t.integer "genre_id", null: false
+    t.text "impression", null: false
+    t.string "sentence", null: false
+    t.text "synopsis", null: false
+    t.string "begining", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_09_24_070123) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
 end
