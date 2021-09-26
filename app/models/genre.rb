bd1d20b245fc1d772207d82b4794f6acb6061d50
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-26 12:38:24 +0900

    ジャンルに医療小説を追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,9 +11,10 @@ class Genre < ActiveHash::Base
     { id: 9,  name: '経済小説' },
     { id: 10, name: '政治小説' },
     { id: 11, name: '歴史小説・時代小説' },
-    { id: 12, name: '児童小説' },
-    { id: 13, name: '官能小説' },
-    { id: 14, name: 'その他' }
+    { id: 12, name: '医療小説' },
+    { id: 13, name: '児童小説' },
+    { id: 14, name: '官能小説' },
+    { id: 15, name: 'その他' }
 ]
   include ActiveHash::Associations
   has_many :users
