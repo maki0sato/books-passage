19cba9cf2844fb3a655f12059edc449e952f3257
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-27 17:56:34 +0900

    commentsコントローラーとcreateアクションを定義

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, book_id: params[:book_id])
+  end
+
+end
