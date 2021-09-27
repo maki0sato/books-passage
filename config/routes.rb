a0db00efd36468469638d5fac23212b3a1b99cf5
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-27 17:54:41 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'books/index'
   root to: "books#index"
-  resources :books
+  resources :books do
+    resources :comments, only: :create
+  end
   resources :users, only: [:show, :edit, :update]
 end
