31f9f899232d61eaf29535eb0d14f83d3f5ed188
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-27 18:32:11 +0900

    バリデーションを設置

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,8 @@ class Comment < ApplicationRecord
 
   belongs_to :book
   belongs_to :user
-
+  
+  validates :user_id,       presence: true
+  validates :book_id,       presence: true
+  validates :comment,       presence: true
 end
