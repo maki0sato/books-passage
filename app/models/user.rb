b43ced718e34231faa84df8c01c39f83748175e0
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-28 10:27:19 +0900

    バリデーションとアソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   belongs_to :genre
   has_many   :books
   has_many   :comments
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
   
   validates :nickname,        presence: true
   validates :genre_id,        presence: true
