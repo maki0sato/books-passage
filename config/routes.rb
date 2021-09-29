06703af2cb09efc89e1d260706caba3b49e148b9
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-09-29 15:39:23 +0900

    ルーティングを設置

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: "books#index"
   resources :books do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users, only: [:show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
