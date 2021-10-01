5dcbd5c01bead0653b5a22f82eabb742372ed74d
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-10-01 14:06:16 +0900

    コメントのルーティングにデストロイを設置

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'books/index'
   root to: "books#index"
   resources :books do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
     collection do
       get 'search'
