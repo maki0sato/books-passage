a08a09e91585d2e471666869bbf94a0d537922ef
maki0sato <89379929+maki0sato@users.noreply.github.com>
2021-10-06 14:56:35 +0900

    ゲストユーザー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   get 'books/index'
   root to: "books#index"
   resources :books do
@@ -15,4 +16,7 @@ Rails.application.routes.draw do
   resources :comments, only: [:destroy]
   resources :users, only: [:show, :edit, :update]
   resources :relationships, only: [:create, :destroy]
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
